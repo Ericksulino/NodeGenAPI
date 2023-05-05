0c43cd21d045e41764e599760db839286db67252
ericksulino <ericksulino@gmail.com>
2023-05-05 14:47:35 -0300

    conectando com o mongodb

--- src/app.js
+++ src/app.js
@@ -1,18 +1,28 @@
 'use strict'
 
 const express = require('express');
+const dotenv = require('dotenv')
 const bodyParser = require('body-parser');
 
 const app = express();
 
+//config do dotenv -variaveis de ambiente
+dotenv.config();
+
 //Carregando rotas
 const indexRoute = require('./routes/indexRoute');
 const productRoute = require('./routes/productRoute');
+
+//Rota com o Banco de dados
+const connectDatabase = require("./database/db")
+
+//Conexão com o Banco de dados
+connectDatabase()
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:false}))
 
 
 app.use('/',indexRoute);
-app.use('/products',create);
+app.use('/products',productRoute);
 module.exports = app;
