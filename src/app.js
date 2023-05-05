16162d33088ceae86c08f53f0fea9bc8d5d8e995
ericksulino <ericksulino@gmail.com>
2023-05-05 13:54:27 -0300

    crud

--- src/app.js
+++ src/app.js
@@ -1,8 +1,13 @@
 'use strict'
 
 const express = require('express');
+const bodyParser = require('body-parser');
+
 const app = express();
 const router = express.Router();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}))
 
 const route = router.get('/', (req, res, next) =>{
     res.status(200).send({
@@ -11,6 +16,24 @@ const route = router.get('/', (req, res, next) =>{
     })
 })
 
-app.use('/',route);
+const create = router.post('/', (req, res, next) =>{
+    res.status(201).send(req.body)
+})
 
+const put = router.put('/:id', (req, res, next) =>{
+    const id = req.params.id;
+    res.status(201).send({
+        id: id,
+        item: req.body})
+})
+
+const del = router.delete('/:id', (req, res, next) =>{
+    res.status(200).send(req.body)
+})
+
+
+app.use('/',route);
+app.use('/products',create);
+app.use('/products',put);
+app.use('/products',del);
 module.exports = app;
